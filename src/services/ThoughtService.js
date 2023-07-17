0423a14b6d5a7b117692f1e192f9874e249f348c
jeffwang <wangjeffrey30@gmail.com>
2023-07-17 19:04:01 -0400

    frontend http requests

---
+++ src/services/ThoughtService.js
@@ -0,0 +1,25 @@
+import { http } from './HttpService'
+
+//Each function corresponds to a specific API endpoint 
+//and makes use of the http() method to send the corresponding 
+//HTTP request.
+
+export function getAllThoughts() {
+    return http().get('/thought')
+}
+
+export function getThoughtById(id) {
+    return http().get(`/thought/${id}`)
+}
+
+export function createThought(thought) {
+    return http().post('/thought', thought)
+}
+
+export function deleteThought(id) {
+    return http().delete(`/thought/${id}`)
+}
+
+export function updateThought(thought) {
+    return http().put('/thought', thought)
+}
--- src/views/thoughts/ThoughtCreate.vue
+++ src/views/thoughts/ThoughtCreate.vue
@@ -1,9 +1,44 @@
 <template>
-    <h1>Thoughts Create</h1>
+  <div>
+      <h1>Create Thought</h1>
+      <form class="custom-form" v-on:submit.prevent="onSubmit">
+          <div class="form-group">
+              <label for="username">Title</label>
+              <input v-model="thought.title" type="text" class="form-control" id="title" placeholder="Title">
+          </div>
+          <div class="form-group">
+              <label for="body">Body</label>
+              <textarea v-model="thought.body" name="body" id="body" cols="30" rows="10"></textarea>
+          </div>
+          <div class="form-group">
+              <button type="submit" class="btn btn-secondary">Submit</button>
+          </div>
+          
+      </form>
+  </div>
 </template>
 
 <script>
-export default {
-  // Component options go here
-}
+
+  import * as thoughtService from '../../services/ThoughtService'
+  export default {
+      name: 'thoughts-create',
+      data() {
+          return {
+              task: {
+                title: '',
+                body: ''
+              }
+          }
+      },
+      methods: {
+          onSubmit: async function() {
+              const request = {
+                  thought: this.thought
+              }
+              await thoughtService.createThought(request)
+              this.$router.push({ name: 'thoughts-all' })
+          }
+      }
+  }
 </script>
