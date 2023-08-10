563e72bd239927bc56fa09c8d1a3915f1937ffd2
jeffwang <wangjeffrey30@gmail.com>
2023-08-10 19:19:06 -0400

    thoughtcreate input control

--- src/views/thoughts/ThoughtCreate.vue
+++ src/views/thoughts/ThoughtCreate.vue
@@ -3,12 +3,12 @@
       <h1>Create Thought</h1>
       <form class="custom-form" v-on:submit.prevent="onSubmit">
           <div class="form-group">
-              <label for="username">Title</label>
-              <input v-model="thought.title" type="text" class="form-control" id="title" placeholder="Title">
+              <label for="username">Title (Max 40 characters)</label>
+              <input v-model="thought.title" type="text" class="form-control" id="title" placeholder="Title" minlength="3" maxlength="40">
           </div>
           <div class="form-group">
               <label for="body">Body</label>
-              <textarea placeholder="Body" v-model="thought.body" name="body" id="body" class="form-control" cols="30" rows="10"></textarea>
+              <textarea placeholder="Body" v-model="thought.body" name="body" id="body" class="form-control" cols="30" rows="10" minlength="3" maxlength="280"></textarea>
           </div>
           <div class="form-group">
               <button type="submit" class="btn btn-secondary">Submit</button>
