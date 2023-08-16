d62807706f0780817d000228db22a39b5849ba74
jeffwang <wangjeffrey30@gmail.com>
2023-08-16 00:11:56 -0400

    apicache temp disable

--- dev-server/api/thought/thought-routes.js
+++ dev-server/api/thought/thought-routes.js
@@ -2,29 +2,29 @@ import express from 'express';
 import * as controller from './thought-controller'
 import * as auth from '../../services/auth-service'
 
-const apicache = require('apicache');
-let cache = apicache.middleware;
+// const apicache = require('apicache');
+// let cache = apicache.middleware;
 
 const router = express.Router();
 
 router.post('/thought', auth.requireLogin, async (req, res) => {
     await controller.create(req, res);
-    apicache.clear('/api/thoughts');
+    // apicache.clear('/api/thoughts');
 })
 
-router.get('/thought', cache('5 minutes'), controller.index)
+router.get('/thought', controller.index)
 
 router.put('/thought', auth.requireLogin, async (req, res) => {
     await controller.update(req, res);
-    apicache.clear('/api/thoughts');
+    //apicache.clear('/api/thoughts');
 })
 
 router.delete('/thought/:id', auth.requireLogin, async (req, res) => {
     await controller.remove(req, res);
-    apicache.clear('/api/thoughts');
+    //apicache.clear('/api/thoughts');
 })
 
-router.get('/thought/:id', cache('5 minutes'), controller.show)
+router.get('/thought/:id', controller.show)
 
 router.put('/thought/:id/likes', auth.requireLogin, controller.updateLikes)
 // no cache for like updates because it modifies server state**
--- prod-server/api/thought/thought-routes.js
+++ prod-server/api/thought/thought-routes.js
@@ -9,23 +9,26 @@ exports.default = void 0;
 var _express = _interopRequireDefault(require("express"));
 var controller = _interopRequireWildcard(require("./thought-controller"));
 var auth = _interopRequireWildcard(require("../../services/auth-service"));
-const apicache = require('apicache');
-let cache = apicache.middleware;
+// const apicache = require('apicache');
+// let cache = apicache.middleware;
 const router = _express.default.Router();
 router.post('/thought', auth.requireLogin, async (req, res) => {
   await controller.create(req, res);
-  apicache.clear('/api/thoughts');
+  // apicache.clear('/api/thoughts');
 });
-router.get('/thought', cache('5 minutes'), controller.index);
+
+router.get('/thought', controller.index);
 router.put('/thought', auth.requireLogin, async (req, res) => {
   await controller.update(req, res);
-  apicache.clear('/api/thoughts');
+  //apicache.clear('/api/thoughts');
 });
+
 router.delete('/thought/:id', auth.requireLogin, async (req, res) => {
   await controller.remove(req, res);
-  apicache.clear('/api/thoughts');
+  //apicache.clear('/api/thoughts');
 });
-router.get('/thought/:id', cache('5 minutes'), controller.show);
+
+router.get('/thought/:id', controller.show);
 router.put('/thought/:id/likes', auth.requireLogin, controller.updateLikes);
 // no cache for like updates because it modifies server state**
 var _default = router;
